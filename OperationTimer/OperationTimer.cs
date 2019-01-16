e2746e8c31217d7e1b308b3dab9028f6634258b8
tylearymf <tylearymf@163.com>
2019-01-16 20:50:59 +0800

    Add Project

---
+++ OperationTimer/OperationTimer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace OperationTimer
+{
+    class OperationTimer : IDisposable
+    {
+        Stopwatch mStopWatch;
+        string mText;
+        int mCollectionCount;
+
+        public OperationTimer(string pText)
+        {
+            PrepareForOperation();
+
+            mText = pText;
+            mCollectionCount = GC.CollectionCount(0);
+            mStopWatch = Stopwatch.StartNew();
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("{0} (GCs={1,3}) {2}", mStopWatch.Elapsed, GC.CollectionCount(0) - mCollectionCount, mText);
+        }
+
+        static void PrepareForOperation()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+    }
+}
---
+++ OperationTimer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperationTimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int cCount = 100000000;
+
+            using (new OperationTimer("List<Int32>"))
+            {
+                var l = new List<int>();
+                for (int i = 0; i < cCount; i++)
+                {
+                    l.Add(i);
+                    var x = l[i];
+                }
+                l = null;
+            }
+
+            using (new OperationTimer("ArrayList"))
+            {
+                var l = new ArrayList();
+                for (Int32 i = 0; i < cCount; i++)
+                {
+                    l.Add(i);
+                    Int32 x = (Int32)l[i];
+                }
+                l = null;
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
